75f55d1cae2b9bb0c29ed46626e83410315f9fc5
alaeddinerami <alaeddinerami7@gmail.com>
2023-10-25 20:20:58 +0100

    done

--- burger.js
+++ burger.js
@@ -1,5 +1,13 @@
-burger = document.querySelector('.burger');
-burger.onclick = function() {
-  navBar = document.querySelector("#navbar");
-  navBar.classList.toggle('active');
-}
+const burger = document.querySelector('.burger');
+const header = document.querySelector('.header-section');
+
+// burger.onclick = function() {
+//   navBar = document.querySelector("#navbar");
+//   navBar.classList.toggle('active');
+// }
+
+
+
+burger.addEventListener("click",()=>{
+	header.classList.toggle("nav-open")
+})
